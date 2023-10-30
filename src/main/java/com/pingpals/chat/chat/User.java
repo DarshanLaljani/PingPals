d3c2676c08eed7593b3d1112ea8cb8a89857ad2f
Darshan <explorerdarshanlaljani@gmail.com>
2023-10-30 22:01:15 +0530

    git commit -m "first commit" git branch -M main git remote add origin https://github.com/DarshanLaljani/PingPals.git git push -u origin main

---
+++ src/main/java/com/pingpals/chat/chat/User.java
@@ -0,0 +1,42 @@
+package com.pingpals.chat.chat;
+
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
